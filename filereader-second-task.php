ac0b6bc46b291743ce2407a291f745a94b9cd82a
khoina <khoina@smartosc.com>
2020-09-10 09:23:25 +0700

    update codes

--- filereader-second-task.php
+++ filereader-second-task.php
@@ -4,15 +4,17 @@ $file = fopen("input.txt","r") or die("Cannot open file!");
 $main_array = preg_split("/[\s,]+/",fread(($file),filesize("input.txt")));
 fclose($file);
 
-if (sizeof($main_array) > 1000){
-    echo "The size of array is larger than 1000. Reducing...\n";
-    $reduce_by = (sizeof($main_array)-1000);
+//Check if the read array has more than 1000 elements (Nmax = 1000). If yes, reduce it back to the maximum of 1000
+$array_size = sizeof($main_array);
+if ($array_size > 1000){
+    echo "The size of array is larger than 1000 (". $array_size ."). Reducing...\n";
+    $reduce_by = ($array_size-1000);
     $main_array = array_slice($main_array, $reduce_by);
     echo "The array has been reduced by " . $reduce_by . ".\n";
 }
+
 //make temp array for counting the time each number appears
 $count_array = array();
-$same_numbers = array();
 
 //Count the main array
 $count_array = array_count_values($main_array);
@@ -21,7 +23,7 @@ $count_array = array_count_values($main_array);
 echo "Numbers that appeared three times or more: ";
 $any_number = false; //key to check if the code found any number that appeared more than 3 times
 foreach ($count_array as $key => $val){
-    //is_numeric is to counter there is a letter (example: d) appeared more than 3 times instead.
+    //is_numeric is to counter if there is a letter (example: d) appeared more than 3 times instead.
     if ($val >= 3 and is_numeric($key)){
         echo $key . " " ;
         $any_number = true;
